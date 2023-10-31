52e73b35ce324a57d20d9c6cd0509c6d01e8cd48
SinBin10 <bincode10@gmail.com>
2023-10-31 22:52:58 +0530

    applying computer choice logic.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const user = document.querySelector('#user-choice');
+const computer = document.querySelector('#computer-choice');
+const computerChoice = ["rock", "paper", "scissors"];
 const rock = document.getElementById('rock');
 rock.addEventListener('click', function() {
     user.innerText = "rock";
+    computer.innerText = computerChoice[Math.floor(Math.random()*3)];
 });
 const paper = document.getElementById('paper');
 paper.addEventListener('click', function() {
     user.innerText = "paper";
+    computer.innerText = computerChoice[Math.floor(Math.random()*3)];
 });
 const scissors = document.getElementById('scissors');
 scissors.addEventListener('click', function() {
     user.innerText = "scissors";
+    computer.innerText = computerChoice[Math.floor(Math.random()*3)];
 });
 
